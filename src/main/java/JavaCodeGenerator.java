cd4c2d267ac1dbff063d4a0b7e8ea39704e66ed0
Sai Sharan Nagulapalli <sharandec7@dhcp-vlan876-9-217-22.vlan861.client.wireless.uic.edu>
2017-12-07 20:30:39 -0600

    Useful Commit

--- src/main/java/JavaCodeGenerator.java
+++ src/main/java/JavaCodeGenerator.java
@@ -11,8 +11,14 @@ import java.util.regex.Pattern;
 
 public class JavaCodeGenerator {
 
+    static int maxNoOfClasses = 1;
+    static int maxNoOfInterfaces = 1;
+    static int maxNoOfImports = 2;
+    static int maxNoOfTypes = 2;
+
     String classbnf[] = {
-            "<import declarations> ::= <import declaration> | <import declarations> <import declaration>",
+
+            "<import declarations> ::= <import declaration>\n | <import declarations> <import declaration>",
             "<import declaration> ::= <single type import declaration> | <type import on demand declaration>",
             "<single type import declaration> ::= import <type import name> ;",
             "<type import on demand declaration> ::= import <package import name>.* ;",
@@ -20,24 +26,100 @@ public class JavaCodeGenerator {
             "<package import name> ::= <import identifier> | <package import name>.<import identifier>",
             "<import identifier> ::= java|lang|util",
 
+            //"<interface declaration> ::= <interface modifiers>? interface <identifier> <extends interfaces>? <interface body>",
+            "<interface declaration> ::= <interface modifiers>? interface <interface type identifier> <interface body>",
+            //"<interface modifiers> ::= <interface modifier> | <interface modifiers> <interface modifier>",
+            "<interface modifiers> ::= <interface modifier>",
+            "<interface modifier> ::= public | abstract",
+            "<extends interfaces> ::= extends <interface type> | <extends interfaces> , <interface type>",
+            "<interface body> ::= { <interface member declarations>? }",
+            "<interface member declarations> ::= <interface member declaration> | <interface member declarations> <interface member declaration>",
+            "<interface member declaration> ::= <constant declaration> | <abstract method declaration>",
+            "<constant declaration> ::= <constant modifiers> <type> <variable declarator> ;",
+            "<constant modifiers> ::= public | static | final",
+            "<abstract method declaration>::= <abstract method modifiers>? <result type> <method declarator> <throws>? ;",
+            "<abstract method modifiers> ::= <abstract method modifier> | <abstract method modifiers> <abstract method modifier>",
+            "<abstract method modifier> ::= public | abstract",
+            "<array initializer> ::= { <variable initializers>? , ? }",
+            "<variable initializers> ::= <variable initializer> | <variable initializers> , <variable initializer>",
+            "<variable initializer> ::= <expression> | <array initializer>",
+            "<variable declarators> ::= <variable declarator> | <variable declarators> , <variable declarator>",
+            "<variable declarator> ::= <variable declarator id> | <variable declarator id> = <variable initializer>",
+            "<variable declarator id> ::= <identifier> | <variable declarator id> [ ]",
+            "<variable initializer> ::= <expression> | <array initializer>",
 
-            "<type declarations> ::= <type declaration> | <type declarations> <type declaration>",
-            "<type declaration> ::= <class declaration> |  ",
-            "<class declaration> ::= <class modifiers>? class <type identifier> <super>? <interfaces>? <class body>",
-            "<class modifiers> ::= <class modifier> | <class modifiers> <class modifier>",
-            "<class modifiers> ::= <class modifier> | <class modifiers> <class modifier>",
-            "<class modifier> ::= public | abstract | final",
+            "<interface type identifier> ::= Interface1 | Interface2 | Interface3",
+
+            "<type declarations> ::= <type declaration>\n | <type declarations> <type declaration>",
+            "<type declaration> ::= <class declaration> |  <interface declaration>",
+            //"<class declaration> ::= <class access specifiers>? <class modifiers>? class <type identifier> <super>? <interfaces>? <class body>",
+            "<class declaration> ::= <class access specifiers>? <class modifiers>? class <class type identifier> <super>? <class body>",
+            "<class modifiers> ::= <class modifier>",
+            "<class access specifiers> ::= <class access specifier>",
+            "<class access specifier> ::= public | private | protected ",
+            "<class modifier> ::= abstract | final | static",
             "<super> ::= extends <class type>",
             "<class type> ::= <type name>",
             "<type name> ::= <type identifier> | <package class name>.<type identifier>",
             "<package class name> ::= <type identifier> | <package name>.<type identifier>",
             "<interfaces> ::= implements <interface type list>",
             "<interface type list> ::= <interface type> | <interface type list> , <interface type>",
-            "<class body> ::= { <class body declarations>? }",
+            "<class body> ::= { \n<class body declarations> }",
             "<class body declarations> ::= <class body declaration> | <class body declarations> <class body declaration>",
             "<class body declaration> ::= <class member declaration> | <static initializer> | <constructor declaration>",
-            "<type identifier> ::= A | B | RandomCodeGenerator"
+
+            "<class type identifier> ::= Class1 | Class2 | Class3",
+
+            "<class member declaration> ::= <field declaration> | <method declaration>",
+            "<static initializer> ::= static <block>",
+            "<constructor declaration> ::= <constructor modifiers>? <constructor declarator> <throws>? <constructor body>",
+            "<constructor modifiers> ::= <constructor modifier> | <constructor modifiers> <constructor modifier>",
+            "<constructor modifier> ::= public | protected | private",
+            "<constructor declarator> ::= <simple constructor name> ( <formal parameter list>? )",
+            "<formal parameter list> ::= <formal parameter> | <formal parameter list> , <formal parameter>",
+            "<formal parameter> ::= <type> <variable declarator id>",
+            "<throws> ::= throws <class type list>",
+            "<class type list> ::= <class type> | <class type list> , <class type>",
+            "<constructor body> ::= { <explicit constructor invocation>? <block statements>? }",
+            "<simple constructor name> ::= <constructor identifier>",
+
+            "<constructor identifier> ::= ",
+
+            "<method declaration> ::= <method header> <method body>",
+            "<method header> ::= <method modifiers>? <result type> <method declarator> <throws>?",
+            "<result type> ::= <type> | void",
+            "<method modifiers> ::= <method modifier> | <method modifiers> <method modifier>",
+            //"<method modifier> ::= public | protected | private | static | abstract | final | synchronized | native",
+            "<method modifier> ::= public | protected | private | static | abstract | final | synchronized",
+            "<method declarator> ::= <method identifier> ( <formal parameter list>? )",
+            "<method body> ::= <block> ",
+            "<block> ::= { <block statements>? }",
+
+            "<method identifier> ::= method1 | method2 | method3 | method4 | method5",
+
+
+            "<field declaration> ::= <field modifiers>? <type> <variable declarators> ;",
+            "<field modifiers> ::= <field modifier> | <field modifiers> <field modifier>",
+            "<field modifier> ::= public | protected | private | static | final | transient | volatile",
+
+            "<type> ::= <primitive type> | <reference type>",
+            "<primitive type> ::= <numeric type> | boolean",
+            "<numeric type> ::= <integral type> | <floating-point type>",
+            "<integral type> ::= byte | short | int | long | char",
+            "<floating-point type> ::= float | double",
+            "<reference type> ::= <class or interface type> | <array type>",
+            "<class or interface type> ::= <class type> | <interface type>",
+            "<class type> ::= <type name>",
+            "<interface type> ::= <type name>",
+            "<array type> ::= <type> [ ]",
+
+            "<simple type name> ::= <identifier>",
+            "<expression name> ::= <identifier> | <ambiguous name> . <identifier>",
+            "<method name> ::= <identifier> | <ambiguous name>. <identifier>",
+            "<ambiguous name>::= <identifier> | <ambiguous name>. <identifier>",
+
     };
+
     HashMap<String, ArrayList<String>> production_rules = new HashMap<>();
 
     void createHM() {
@@ -55,7 +137,6 @@ public class JavaCodeGenerator {
             String value = production_rules.get(name).toString();
             //System.out.println("key:" + key + "value:" + value);
         }
-
     }
 
     void randomGrammarPicker() {
@@ -76,25 +157,38 @@ public class JavaCodeGenerator {
             String found = matcher.group(0).trim();
             System.out.println("found:" + found);
 
-
             if (production_rules.get(found) != null && !found.equals("<type declarations>")) {
 
                 ArrayList<String> rules_associated = production_rules.get(found);
+
                 if (!rules_associated.isEmpty()) {
+
                     Random r = new Random();
                     String replace = rules_associated.get(r.nextInt(rules_associated.size()));
-                    System.out.println("replace:" + replace);
-                    if (found.equals("<import identifier>")) {
-                        rules_associated.remove(replace);
-                        for (String str : rules_associated) {
-                            System.out.println("------------>>>>>>" + str);
-                        }
-                        production_rules.put(found, rules_associated);
-                        import_declaration = import_declaration.replaceFirst(found, replace);
-                        import_declaration = generateImport(import_declaration);
 
-                    } else if (!found.equals("<import identifier>")) {
-                        import_declaration = import_declaration.replaceFirst(found, replace);
+                    //check if allowed to replace and form
+                    if (checkConfig(found, replace)) {
+
+                        //if yes then replace
+                        if (found.equals("<import identifier>")) {
+
+                            System.out.println("replace:" + replace);
+
+                            rules_associated.remove(replace);
+                            for (String str : rules_associated) {
+                                System.out.println(">>>>>>------------" + str);
+                            }
+                            production_rules.put(found, rules_associated);
+                            import_declaration = import_declaration.replaceFirst(found, replace);
+                            import_declaration = generateImport(import_declaration);
+
+                        } else if (!found.equals("<import identifier>")) {
+                            import_declaration = import_declaration.replaceFirst(found, replace);
+                            import_declaration = generateImport(import_declaration);
+                        }
+
+                    } else {
+                        import_declaration = import_declaration.replaceFirst(found, "");
                         import_declaration = generateImport(import_declaration);
                     }
                 }
@@ -102,6 +196,44 @@ public class JavaCodeGenerator {
             }
         }
         return import_declaration;
+    }
+
+    private boolean checkConfig(String found, String replace) {
+
+        switch (found) {
+            case "<type declarations>":
+                if (replace.contains("<type declaration>")) {
+                    maxNoOfTypes = maxNoOfTypes - 1;
+                    if (maxNoOfTypes < 0) {
+                        return false;
+                    } else return true;
+                }
+                break;
+            case "<import declarations>":
+                if (replace.contains("<import declaration>")) {
+                    maxNoOfImports = maxNoOfImports - 1;
+                    if (maxNoOfImports < 0) {
+                        return false;
+                    } else return true;
+                }
+                break;
+            case "<type declaration>":
+                if (replace.contains("<class declaration>")) {
+                    maxNoOfClasses = maxNoOfClasses - 1;
+                    if (maxNoOfClasses < 0) {
+                        return false;
+                    } else return true;
+                } else if (replace.contains("<interface declaration>")) {
+                    maxNoOfInterfaces = maxNoOfInterfaces - 1;
+                    if (maxNoOfInterfaces < 0) {
+                        return false;
+                    } else return true;
+                }
+                break;
+            default:
+                return true;
+        }
+        return true;
     }
 
     String generateClass(String type_declarations) throws ParseException {
@@ -115,26 +247,50 @@ public class JavaCodeGenerator {
 
             String found = matcher.group(0).trim();
             System.out.println("found:" + found);
+
             if (production_rules.get(found) != null) {
 
                 type_declarations = optionalCFG(type_declarations);
                 ArrayList<String> rules_associated = production_rules.get(found);
                 Random r = new Random();
                 String replace = "";
+
                 if (rules_associated.size() > 0) {
+
                     replace = rules_associated.get(r.nextInt(rules_associated.size())).trim();
                     System.out.println("replace: " + replace);
-                    if (found.equals("<type identifier>")) {
-                        rules_associated.remove(replace);
-                    /*for (String str : rules_associated) {
-                        System.out.println("------------>>>>>>" + str);
-                    }*/
-                        production_rules.put(found, rules_associated);
-                        type_declarations = type_declarations.replaceFirst(found, replace);
-                        type_declarations = generateClass(type_declarations).trim();
+
+                    if (checkConfig(found, replace)) {
+                        System.out.println(maxNoOfTypes);
+
+                        if (found.equals("<interface type identifier>") || found.equals("<class type identifier>")) {
+
+                            rules_associated.remove(replace);
+                            /*for (String str : rules_associated) {
+                                System.out.println("------------>>>>>>" + str);
+                            }*/
+                            production_rules.put(found, rules_associated);
+
+                            //add class names to constructors
+                            if (found.equals("<class type identifier>")) {
+                                ArrayList<String> constructor_list = production_rules.get("<constructor identifier>");
+                                constructor_list.add(replace);
+                                for (String str : constructor_list) {
+                                    System.out.println("------------>>>>>>" + str);
+                                }
+                                production_rules.put("<constructor identifier>", constructor_list);
+                            }
+                            type_declarations = type_declarations.replaceFirst(found, replace);
+                            type_declarations = generateClass(type_declarations).trim();
+
+                        } else {
+
+                            type_declarations = type_declarations.replaceFirst(found, replace).trim();
+                            type_declarations = generateClass(type_declarations).trim();
+                        }
                     } else {
-                        type_declarations = type_declarations.replaceFirst(found, replace).trim();
-                        type_declarations = generateClass(type_declarations).trim();
+                        type_declarations = type_declarations.replaceFirst(found, "");
+                        type_declarations = generateImport(type_declarations);
                     }
                 }
             }
@@ -156,7 +312,10 @@ public class JavaCodeGenerator {
             Random r = new Random();
             int check = r.nextInt(2);
             if (check == 0) {
-                type_declarations = type_declarations.replaceFirst(found, "");
+                if (found.equals("<formal parameter>?"))
+                    type_declarations = type_declarations.replaceFirst(", <formal parameter>\\?", "");
+                else
+                    type_declarations = type_declarations.replaceFirst(found, "");
             }
             System.out.println("replaced Optional: " + check + " : " + type_declarations);
         }
@@ -164,21 +323,62 @@ public class JavaCodeGenerator {
     }
 
     public static void main(String args[]) throws ParseException, IOException {
+
         JavaCodeGenerator jcg = new JavaCodeGenerator();
         jcg.createHM();
         jcg.randomGrammarPicker();
         String s = "<package declaration> <import declarations>  <type declarations> ";
+
         //s = "<type declarations>";
-        String got = "";
-        s = s.replace("<package declaration>",jcg.generatePackage(s));
+
+        s = s.replace("<package declaration>", jcg.generatePackage(s));
         s = jcg.generateImport(s);
         s = jcg.generateClass(s);
+        //s = jcg.generateClassBody(s);
+
         s = s.replaceAll("\\?", "");
-        System.out.println("\n\n\n\n\n\nGOT: " + s);
-        File f = new File("/Users/sharandec7/Desktop/vishalkumar_malli_guneshekaran_saisharan_nagulapalli_reenamary/src/main/java", "B" + ".java");
+        s = s.replaceAll(";", ";\\\n");
+
+        System.out.println("\n\n\nGENERATED CODE: " + s);
+
+        String publicClassname = getFileNameFromCodeGenerated(s);
+        System.out.println("\n\n\n\n\nFile Name: " + publicClassname);
+
+        jcg.printToFile(s, publicClassname);
+    }
+
+    private static String getFileNameFromCodeGenerated(String generatedCode) {
+
+        ArrayList<String> t = new ArrayList<String>();
+        for (String r : generatedCode.split(" ")) {
+            if (!r.trim().isEmpty()) {
+                t.add(r.trim());
+                System.out.println("TOKEN: " + r.trim());
+            }
+        }
+        if (t.indexOf("class") != -1) {
+            int Position = t.indexOf("class");
+            return t.get(Position + 1);
+        } else if (t.indexOf("interface") != -1) {
+            int Position = t.indexOf("interface");
+            return t.get(Position + 1);
+        }
+        return "NotFound";
+    }
+
+    void printToFile(String generatedCode, String filename) throws IOException {
+
+        File f = new File("src/main/java", filename + ".java");
+
         if (!f.exists())//check if the file already exists
             f.createNewFile();
-        FileUtils.writeStringToFile(f, s);
+        FileUtils.writeStringToFile(f, generatedCode);
+    }
+
+
+    private String generateClassBody(String class_body_declaration) {
+
+        return "";
     }
 
     private String generatePackage(String s) {
