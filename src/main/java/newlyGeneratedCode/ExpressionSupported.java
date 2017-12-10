a2ed4ec09046b9d93905147de565a9b8e6b2c9c3
rputho2 <rputho2@uic.edu>
2017-12-10 10:49:11 -0600

    Reena Fix

---
+++ src/main/java/newlyGeneratedCode/ExpressionSupported.java
@@ -0,0 +1,8 @@
+package newlyGeneratedCode;
+ import java.net.*;
+import java.io.*;
+    class ExpressionSupported  {
+protected  ( ParameterFocus u ,  )  {
+ <explicit constructor invocation> <block statements> 
+} <class body declaration>
+}
